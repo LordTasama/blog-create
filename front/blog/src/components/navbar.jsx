f3d39a17ec84f521ed8f7b8b7f966b0ff2300ea4
LordTasama <tasamaperez2005@gmail.com>
2024-06-29 19:02:37 -0500

    Más cambios en el front

--- front/blog/src/components/navbar.jsx
+++ front/blog/src/components/navbar.jsx
@@ -1,13 +1,18 @@
 // React
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // Bootstrap
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Carousel from "react-bootstrap/Carousel";
-import Dropdown from "react-bootstrap/Dropdown";
-import { Image } from "react-bootstrap";
+import {
+  Container,
+  Nav,
+  Navbar,
+  Carousel,
+  Dropdown,
+  Image,
+  Button,
+  Modal,
+} from "react-bootstrap";
 
 // Imagenes
 import firstSlide from "../assets/media/first-slide.jpg";
@@ -17,116 +22,156 @@ import defaultImg from "../assets/media/default-avatar-icon-of-social-media-user
 
 // FontAwesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faPen, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
 function NavBar() {
+  const location = useLocation();
   const changePage = (element) => {
     document.querySelectorAll(".nav-link-custom").forEach((e) => {
       e.classList.remove("focus-link-custom");
     });
     element.currentTarget.classList.toggle("focus-link-custom");
   };
-  return (
-    <Container fluid>
-      <Carousel
-        className="carrousel-container mx-auto mt-5"
-        prevIcon={false}
-        nextIcon={false}
-      >
-        <Carousel.Item interval={2000}>
-          <Image src={firstSlide} className="img-slide" />
-          <Carousel.Caption className="bg-transparent">
-            <p className="bg-transparent h5">Disfruta de mi blog</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={2000}>
-          <Image src={secondSlide} className="img-slide" />
-          <Carousel.Caption className="bg-transparent">
-            <p className="bg-transparent h5">
-              Tal vez puedas conocerme un poco más
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Image src={thirdSlide} className="img-slide" />
-          <Carousel.Caption className="bg-transparent">
-            <p className="bg-transparent h5">¿Qué esperas?</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      <Navbar bg="transparent" expand="lg" className="justify-content-center">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse
-          id="basic-navbar-nav"
-          className="justify-content-center mt-2"
-        >
-          <Nav>
-            <Nav.Link
-              as={Link}
-              to="/"
-              className={"text-center nav-link-custom focus-link-custom"}
-              onClick={(event) => changePage(event)}
-            >
-              Inicio
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/publications"
-              className="text-center nav-link-custom"
-              onClick={(event) => changePage(event)}
-            >
-              Publicaciones
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
 
-        <Dropdown className="mx-1">
-          <Dropdown.Toggle
-            id="custom-dropdown-button"
-            as="div"
-            className="custom-dropdown-toggle"
+  useEffect(() => {
+    let index = 0;
+    let element = "";
+    if (location.pathname == "/publications") {
+      index = 1;
+    }
+    document.querySelectorAll(".nav-link-custom").forEach((e, i) => {
+      e.classList.remove("focus-link-custom");
+      element = i == index ? e : element;
+    });
+
+    element.classList.toggle("focus-link-custom");
+  }, []);
+  const [logOutModal, setLogOutModal] = useState(false);
+
+  return (
+    <>
+      <Container fluid>
+        <Carousel
+          className="carrousel-container mx-auto mt-5"
+          prevIcon={false}
+          nextIcon={false}
+        >
+          <Carousel.Item interval={2000}>
+            <Image src={firstSlide} className="img-slide" />
+            <Carousel.Caption className="bg-transparent">
+              <p className="bg-transparent h5">Disfruta de mi blog</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={2000}>
+            <Image src={secondSlide} className="img-slide" />
+            <Carousel.Caption className="bg-transparent">
+              <p className="bg-transparent h5">
+                Tal vez puedas conocerme un poco más
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={thirdSlide} className="img-slide" />
+            <Carousel.Caption className="bg-transparent">
+              <p className="bg-transparent h5">¿Qué esperas?</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        <Navbar bg="transparent" expand="lg" className="justify-content-center">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-center mt-2"
           >
-            <Image
-              src={defaultImg}
-              roundedCircle
-              alt="Imagen de perfil"
-              className="foto-perfil"
-            />
-          </Dropdown.Toggle>
-          <Dropdown.Menu align="end" className="align-items-center">
-            <Dropdown.ItemText
-              style={{ backgroundColor: "transparent" }}
-              className="text-center"
+            <Nav>
+              <Nav.Link
+                as={Link}
+                to="/"
+                className={"text-center nav-link-custom"}
+                onClick={(event) => changePage(event)}
+              >
+                Inicio
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/publications"
+                className="text-center nav-link-custom"
+                onClick={(event) => changePage(event)}
+              >
+                Publicaciones
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+
+          <Dropdown className="mx-1">
+            <Dropdown.Toggle
+              id="custom-dropdown-button"
+              as="div"
+              className="custom-dropdown-toggle"
             >
-              Anderson Tasama
-            </Dropdown.ItemText>
-            <Dropdown.Divider />
-            <Dropdown.Item eventKey="1" className="d-flex align-items-center">
-              <FontAwesomeIcon
-                icon={faPen}
-                className="me-1 bg-secondary text-light"
-                style={{
-                  borderRadius: "50%",
-                  padding: "5%",
-                }}
+              <Image
+                src={defaultImg}
+                roundedCircle
+                alt="Imagen de perfil"
+                className="foto-perfil"
               />
-              Editar perfil
-            </Dropdown.Item>
-            <Dropdown.Item eventKey="2" className="d-flex align-items-center">
-              <FontAwesomeIcon
-                icon={faRightFromBracket}
-                className="me-1 bg-secondary text-light"
-                style={{
-                  borderRadius: "50%",
-                  padding: "5%",
-                }}
-              />
-              Cerrar sesión
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Navbar>
-    </Container>
+            </Dropdown.Toggle>
+            <Dropdown.Menu align="end" className="align-items-center">
+              <Dropdown.ItemText
+                style={{ backgroundColor: "transparent" }}
+                className="text-center"
+              >
+                Anderson Tasama
+              </Dropdown.ItemText>
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="1" className="d-flex align-items-center">
+                <FontAwesomeIcon
+                  icon={faPen}
+                  className="me-1 bg-secondary text-light"
+                  style={{
+                    borderRadius: "50%",
+                    padding: "5%",
+                  }}
+                />
+                Editar perfil
+              </Dropdown.Item>
+              <Dropdown.Item
+                eventKey="2"
+                className="d-flex align-items-center"
+                onClick={() => setLogOutModal(true)}
+              >
+                <FontAwesomeIcon
+                  icon={faRightFromBracket}
+                  className="me-1 bg-secondary text-light"
+                  style={{
+                    borderRadius: "50%",
+                    padding: "5%",
+                  }}
+                />
+                Cerrar sesión
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar>
+      </Container>
+      {/* Modals */}
+      <Modal show={logOutModal}>
+        <Modal.Header>
+          <Modal.Title>Cerrar sesión</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Esta acción cerrará la sesión actual, deberás iniciar una nueva sesión
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setLogOutModal(false)}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={() => setLogOutModal(false)}>
+            Aceptar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
--- front/blog/src/components/publications.jsx
+++ front/blog/src/components/publications.jsx
@@ -1,22 +1,72 @@
 // Bootstrap
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import Button from "react-bootstrap/Button";
-
+import { Form, Button, Modal, FloatingLabel } from "react-bootstrap";
 // FontAwesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+// React
+import { useState } from "react";
+
 const Publications = () => {
+  const [addPublication, setAddPublication] = useState(false);
   return (
     <>
       <Button
         variant="secondary"
         style={{ position: "fixed", right: "2%", bottom: "7%" }}
         title="Crear publicación"
+        onClick={() => setAddPublication(true)}
       >
         <FontAwesomeIcon icon={faPlus} size="lg" className="bg-transparent" />
       </Button>
+      {/* Modals */}
+      <Modal show={addPublication}>
+        <Modal.Header>
+          <Modal.Title>Crear publicación</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <FloatingLabel
+              controlId="titleAddPublication"
+              label="Título"
+              className="mb-3"
+            >
+              <Form.Control type="text" placeholder="Agrega un título" />
+            </FloatingLabel>
+            <FloatingLabel
+              controlId="descriptionAddPublication"
+              label="Descripción"
+              className="mb-3"
+            >
+              <Form.Control
+                as="textarea"
+                placeholder="Agrega una descripción"
+              />
+            </FloatingLabel>
+            <>
+              <Form.Control
+                id="fileAddPublication"
+                type="file"
+                className="d-none"
+              />
+              <Form.Label
+                for="fileAddPublication"
+                className="btn btn-primary d-flex justify-content-center"
+              >
+                Subir imagen
+              </Form.Label>
+            </>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setAddPublication(false)}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={() => setAddPublication(false)}>
+            Crear
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
